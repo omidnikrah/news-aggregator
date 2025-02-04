079c54ba12989d98d1fab64516edbf0ad89d3ed0
Omid Nikrah <omidnikrah@gmail.com>
2025-02-04 17:37:59 +0330

    ⚡️ fix: add debounce on filters

--- src/queries/useNews.ts
+++ src/queries/useNews.ts
@@ -1,11 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { newsService } from "@src/services";
+import { useDebounce } from "@src/hooks";
 
 export const useNews = (sources: TSources[], filters?: NewsFilters) => {
   const selectedSources: TSources[] = sources.length > 0 ? sources : ["nyt", "guardian", "newsapi"];
 
+  const debouncedFilters = useDebounce(filters, 300);
+
   return useQuery<Article[]>({
-    queryKey: ["news", selectedSources, filters],
-    queryFn: () => newsService.fetchNewsFromSources(selectedSources, filters),
+    queryKey: ["news", selectedSources, debouncedFilters],
+    queryFn: () => newsService.fetchNewsFromSources(selectedSources, debouncedFilters),
   });
 };
